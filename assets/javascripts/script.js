17136861c8eb0a9b334ba876e97c6fb6eda2196f
Patrick Azevedo <patrick.calazans@hotmail.com>
2024-02-06 19:58:29 -0300

    atividade-06-02-24

--- assets/javascripts/script.js
+++ assets/javascripts/script.js
@@ -1,26 +1,56 @@
+const pokemonListElement = document.querySelectorAll(".section-pokemons-div")
+
+const inputSearch = document.getElementById('input-search')
+const btnSearch = document.getElementById('button-search')
+
+const pokemonImg = document.querySelector('.pokemon-img-search')
+const pokemonId = document.querySelector('.pokemon-id-search')
+const pokemonName = document.querySelector('.pokemon-name-search')
+
 const btnBack = document.getElementById('button-back')
 const btnForward = document.getElementById('button-forward')
-const pokemonListElement = document.querySelectorAll(".section-pokemons-div");
 
-let currentIndex = 0;
+let currentIndex = 0
 
-const updatePokemonElement = (element, pokemon) => {
-    const imgElement = element.querySelector("img");
-    imgElement.setAttribute("src", `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`);
+btnSearch.addEventListener('click', () => {
+    searchPokemon()
+})
 
-    const pElement = element.querySelector(".pokemon-id");
-    pElement.textContent = pokemon.id;
-
-    const h4Element = element.querySelector(".pokemon-name");
-    h4Element.textContent = pokemon.name;
+const searchPokemon = () => {
+    if(inputSearch.value.trim()) {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${inputSearch.value.trim().toLowerCase()}`)
+        .then((res) => res.json())
+        .then((data) => {  
+            pokemonImg.setAttribute('src', `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png`),
+            pokemonId.textContent = data.id,
+            pokemonName.textContent = data.name.charAt(0).toUpperCase() + data.name.slice(1)
+        }).catch(() => {
+            alert(`Ocorreu um erro com a sua busca.`)
+        })
+    } else {
+        alert('Nome inválido!')
+        return
+    }
+    inputSearch.value = ''
 }
 
-const fetchAndUpdatePokemonList = () => {
+const updatePokemonElement = (element, pokemon) => {
+    const imgElement = element.querySelector("img")
+    imgElement.setAttribute("src", `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`)
+
+    const pElement = element.querySelector(".pokemon-id")
+    pElement.textContent = pokemon.id
+
+    const h4Element = element.querySelector(".pokemon-name")
+    h4Element.textContent = pokemon.name
+}
+
+const fetchPokemonList = () => {
     fetch("https://pokeapi.co/api/v2/pokemon?limit=125")
     .then((response) => response.json())
     .then((data) => {
-        const pokemonArray = data.results;
-        currentIndex = Math.min(currentIndex, pokemonArray.length - 8);
+        const pokemonArray = data.results
+        currentIndex = Math.min(currentIndex, pokemonArray.length - 8)
 
         for (let i = currentIndex; i < currentIndex + 8; i++) {
             if (pokemonArray[i]) {
@@ -37,14 +67,14 @@ const fetchAndUpdatePokemonList = () => {
     })
 }
 
-fetchAndUpdatePokemonList();
+fetchPokemonList();
 
 btnBack.addEventListener("click", () => {
-    currentIndex = Math.max(0, currentIndex - 8);
-    fetchAndUpdatePokemonList();
+    currentIndex -= 8
+    fetchPokemonList();
 });
 
 btnForward.addEventListener("click", () => {
-    currentIndex = Math.min(currentIndex + 8, 125 - 8);
-    fetchAndUpdatePokemonList();
+    currentIndex += 8 
+    fetchPokemonList();
 });
